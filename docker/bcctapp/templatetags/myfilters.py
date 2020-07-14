a68b3f221f95ffc686449defa1a0907254e79ad7
JoaoAbelha <jpabelha@hotmail.com>
2020-07-14 21:55:44 +0100

    profile ui

--- docker/bcctapp/templatetags/myfilters.py
+++ docker/bcctapp/templatetags/myfilters.py
@@ -3,5 +3,7 @@ from django import template
 register = template.Library()
 
 @register.filter(name='addclass')
-def addclass(value, arg):
-    return value.as_widget(attrs={'class': arg})
+def addclass(field, class_attr):
+    return field.as_widget(attrs={'class': class_attr})
+
+ 
