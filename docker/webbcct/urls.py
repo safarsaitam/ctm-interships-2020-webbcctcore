9b22cd5dd788922ebf5608e49cdb57e3d67609c3
JoaoAbelha <jpabelha@hotmail.com>
2020-07-30 09:23:27 +0100

    ui fix

--- docker/webbcct/urls.py
+++ docker/webbcct/urls.py
@@ -24,9 +24,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('bcctapp.urls')),
-    path('cinderella/', include('cinderella.urls')),
+    path('cinderella/', include('cinderella.urls')), 
     path('register/', user_views.register, name="register"),
-    path('profile/', user_views.profile, name="profile"),
+    path('profile/', user_views.profile, name="profile"), #done
     path('accounts/',include('allauth.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name="login"),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name="logout"),
